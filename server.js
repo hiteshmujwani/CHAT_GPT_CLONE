eeb4cc51313467cf2547aa0e95cdc758a3ebe7e8
Hitesh Mujwani <145272517+hiteshmujwani@users.noreply.github.com>
2024-02-21 22:19:48 +0530

    fixed

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./DB/mongo.js";
 import authRoute from "./routes/authRoute.js";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 const app = express();
 
 dotenv.config();
@@ -13,14 +13,14 @@ const PORT = process.env.PORT || 8080;
 //databse connectivity
 connectDB();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./client/build/")));
+// app.use(express.static(path.join(__dirname, "./client/build/")));
 app.use((req, res, next) => {
   // Set the Access-Control-Allow-Origin header to allow requests from any origin
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -43,9 +43,9 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/v1/auth", authRoute);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
